5bd7988292ca6c3a8f5e7c344add87573dc802c2
hbaltz <hugo.baltz@ensg.eu>
2016-02-04 22:30:32 +0100

    Gestion des collisions ++

--- src/js/utilitaires.js
+++ src/js/utilitaires.js
@@ -1,5 +1,5 @@
 function intersection(y1, h1, y2, h2, ok) {
-	return ((y2+h2 > y1 && h2 < y1+h1) && ok ==1);
+	return ((y1+h1 == y2+h2 || y1+h1 == y2 || y1 == y2 || y1 == y2+h2 ) && ok ==1); // Beacoup de test pour assurer la détéction d'intesrsection
 }
 
 function dansPorte(x1, w1, x2, w2) {
