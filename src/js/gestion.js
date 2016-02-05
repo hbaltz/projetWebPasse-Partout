d74b2492634d92b8b5f7ccd4ba26a6986664cf8d
hbaltz <hugo.baltz@ensg.eu>
2016-02-05 16:59:53 +0100

    Nouvelle partie
    Gestion du fait de pouvoir refaire une partie une fois que l'on a perdu

--- src/js/gestion.js
+++ src/js/gestion.js
@@ -22,113 +22,136 @@ var rfc = 1; // Temps sépérant la vérification des collisions
 
 var pptX, pptY, pptH, pptW, murX, murY, murH, porteX, porteY, porteH, porteW, testPor, testMur;
 var score; 
+var ok = 1;
 
-$(function() {
+window.onload = function() {
+	debut_partie();
+}
 
-	var ok = 1;
-	function obstacle(){
-		// Gestion de l'accélération :
-		vam += am;
-		am += 1;
-
-		//Déplacement et apparation des murs :
-		$('#mur').animate({top: '+=' + vam}, tm, 'linear', function(){
-
-			var porteW = Math.floor(Math.random()*tap)+20;
-
-			var porteX = Math.floor(Math.random()*pm);
-
-			$('#mur').css('top', 0);
-
-			$('#porte').css('width',porteW);
-			$('#porte').css('margin-left',porteX);
-
-			ok = 1;
-			
-
-		});
-	};
-
-	function deplace(){
-		// Gestion de l'accélération :
-		/*
-		vdf += af;
-		af += 1;
-		*/
-
-		// Animation du fond :
-		$('.fond').animate({
-			top: '+='+vdf
-			},
-			tf, //Temps du deplacement
-			'linear', // Mode de deplacement
-
-
-			// On relance la fonction une fois que le deplacement est fini :
-			function(){
-				$('.fond').css('top', -vdf);
-				deplace();
-				obstacle();
-			});
-		}
-
-		// Ajout evenement pour faire bouger passe-partout a l'aide des touches droite et gauche :
-		$(document).keydown(function(e){
-			if (e.which == 39){
-				pptX = parseInt($('#ppt').css('left'));
-				if (pptX < zdj)
-				$('#ppt').css('left', pptX + tcur);
-			}
-
-			if (e.which == 37){
-				pptX = parseInt($('#ppt').css('left'));
-				if (pptX > hdj)
-				$('#ppt').css('left', pptX - tcur);
-			}
-	});
-
-	// Fonction pour surveiller la collision entre passe-partout et les murs :
-	function collision(){
-
-		pptY = parseInt($('#ppt').css('top'));
-		pptH = parseInt($('#ppt').css('height'));
-
-		murY = parseInt($('#mur').css('top'));
-		murH = parseInt($('#mur').css('height'));
-		
-		porteY = parseInt($('#mur').css('top'));
-		porteH = parseInt($('#porte').css('height'));
-
-		testMur = intersection(pptY, pptH, murY, murH, ok);
-		
-		if (testMur){
-			pptX = parseInt($('#ppt').css('left'));
-			pptW = parseInt($('#ppt').css('width'));
-
-			murX = parseInt($('#mur').css('left'));
-
-			porteX = parseInt($('#porte').css('margin-left')) + murX;
-			porteW = parseInt($('#porte').css('width'));
-
-			testPor = dansPorte(pptX, pptW, porteX, porteW);
-
-			if(testPor){
-				score = parseInt($('#score').text()) + 1;
-				$('#score').text(score);
-				ok = 0;
-			} else {
-				finJeu();
-			}
-		}  
-	}
-
-	function finJeu(){
-		$('#partie').hide();
-		$('#partie').html('');
-		$('#fin').show();
-		$('#inf_score').text(score);
-	}
-
+// Function pour initialiser la partie :
+function debut_partie(){
+	$('#partie').html('<p> Collisions : <span id="info">0</span> </p><p> Score : <span id="score">0</span> </p>');
+	$('<div id="jeu">').appendTo('#partie');
+	$('<img id="fond1" class="fond" src="img/route.png"><img id="fond2" class="fond" src="img/route.png">').appendTo('#jeu');
+	$('<img id="ppt" src="img/ppt.png"> <div id = "mur"></div>').appendTo('#jeu');
+	$('<div id = "porte">').appendTo('#mur');
 	deplace();
 	setInterval(collision, rfc); // Verification des collisons toutes les rfc ms
-}); 
+}
+
+// Gestion des obstacles :
+function obstacle(){
+	// Gestion de l'accélération :
+	vam += am;
+	am += 1;
+
+	//Déplacement et apparation des murs :
+	$('#mur').animate({top: '+=' + vam}, tm, 'linear', function(){
+
+		var porteW = Math.floor(Math.random()*tap)+20;
+
+		var porteX = Math.floor(Math.random()*pm);
+
+		$('#mur').css('top', 0);
+
+		$('#porte').css('width',porteW);
+		$('#porte').css('margin-left',porteX);
+
+		ok = 1;
+		
+	});
+};
+
+// Gestion du déplacement du fond
+function deplace(){
+	// Gestion de l'accélération :
+	/*
+	vdf += af;
+	af += 1;
+	*/
+
+	// Animation du fond :
+	$('.fond').animate({
+		top: '+='+vdf
+		},
+		tf, //Temps du deplacement
+		'linear', // Mode de deplacement
+
+
+		// On relance la fonction une fois que le deplacement est fini :
+		function(){
+			$('.fond').css('top', -vdf);
+			deplace();
+			obstacle();
+		}
+	);
+}
+
+// Fonction pour surveiller la collision entre passe-partout et les murs :
+function collision(){
+
+	pptY = parseInt($('#ppt').css('top'));
+	pptH = parseInt($('#ppt').css('height'));
+
+	murY = parseInt($('#mur').css('top'));
+	murH = parseInt($('#mur').css('height'));
+	
+	porteY = parseInt($('#mur').css('top'));
+	porteH = parseInt($('#porte').css('height'));
+
+	testMur = intersection(pptY, pptH, murY, murH, ok);
+	
+	if (testMur){
+		pptX = parseInt($('#ppt').css('left'));
+		pptW = parseInt($('#ppt').css('width'));
+
+		murX = parseInt($('#mur').css('left'));
+
+		porteX = parseInt($('#porte').css('margin-left')) + murX;
+		porteW = parseInt($('#porte').css('width'));
+
+		testPor = dansPorte(pptX, pptW, porteX, porteW);
+
+		if(testPor){
+			score = parseInt($('#score').text()) + 1;
+			$('#score').text(score);
+			ok = 0;
+		} else {
+			finJeu();
+		}
+	}  
+}
+
+// Gestion du game over :
+function finJeu(){
+	$('#partie').hide();
+	$('#partie').html('');
+	$('#fin').show();
+	$('#inf_score').text(score);
+}
+
+// Pour relancer une partie :
+$( "#nouvelle_partie").click(function(e){
+	$('#fin').hide();
+	$('#partie').show();
+	debut_partie();
+});
+
+/**
+Gestion des déplacement
+**/
+
+// Ajout evenement pour faire bouger passe-partout a l'aide des touches droite et gauche :
+$(document).keydown(function(e){
+	if (e.which == 39){
+		pptX = parseInt($('#ppt').css('left'));
+		if (pptX < zdj)
+		$('#ppt').css('left', pptX + tcur);
+	}
+
+	if (e.which == 37){
+		pptX = parseInt($('#ppt').css('left'));
+		if (pptX > hdj)
+		$('#ppt').css('left', pptX - tcur);
+	}
+});
